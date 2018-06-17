fb16d337c469a6a7814fc27d350a0f2626e06d28
charleyustccs <charleyustccs@gmail.com>
2018-06-17 22:43:07 +0800

    update bm-v2

--- donut/block_manager_linux/archive.py
+++ donut/block_manager_linux/archive.py
@@ -3,13 +3,17 @@ import posix
 import shutil
 import json
 import time
+import zipfile
 
 
 class Archive(object):
     """set the files' modified time stamp to a uniform time stamp
+    zip the folder into {gcc-8-base}_donut.zip
+    unzip {gcc-8-base}_donut.zip to {} folder
 
-    :param path: the direct file's path ("{}/test".format(os.getcwd()))
-    :param pack_type: zip, tar, bz2...
+    :param path: top/donut
+    :param pkg_name: gcc-8-base
+    :param pack_type: zip
     :param modified_time: "1 Jan 18"
     """
     def __init__(self, path, pkg_name, pack_type, modified_time):
@@ -17,16 +21,20 @@ class Archive(object):
         self.pkg_name = pkg_name
         (self.top_path, self.name) = os.path.split(path)
         self.pack_type = pack_type
-        self.time_path = self.top_path + "/TIME_Record_{}.json".format(self.name)
         self.m_time_stamp = time.mktime(time.strptime(modified_time, "%d %b %y"))
         self.a_time_stamp = self.m_time_stamp
 
+        # TODO: discuss
+        self.time_path = self.top_path + "/TIME_Record_{}.json".format(self.name)
+
+    # TODO: discuss
     def read_cm_time(self):
         """Abandon!!!"""
         with open(self.time_path, 'r') as f:
             data = json.load(f)
             return data
 
+    # TODO: discuss
     def set_original_time(self):
         # TODO: get the way to modify file's created time
         """set the file's access time and modified time to his original time"""
@@ -52,11 +60,21 @@ class Archive(object):
         posix.utime(self.path, (self.a_time_stamp, self.m_time_stamp))
 
     def pack_it(self):
-        # TODO: better to fix it
         """the archive file is saved into the work_place, but this avoids naming conflict on the other hand"""
-        shutil.make_archive("{}/{}_archive".format(os.getcwd(), self.pkg_name), "zip", r"{}".format(self.path))
+        shutil.make_archive("{}/{}_donut".format(os.getcwd(), self.pkg_name), "zip", r"{}".format(self.path))
+
+    @staticmethod
+    def un_zip(file_name, dst):
+        """unzip zip file"""
+        zip_file = zipfile.ZipFile(file_name)
+        if os.path.isdir(dst):
+            pass
+        else:
+            os.mkdir(dst)
+        for names in zip_file.namelist():
+            zip_file.extract(names, dst+'/')
+        zip_file.close()
 
     def unpack_it(self):
-        # TODO: a part of installing, or rename the recovered archive file
-        shutil.unpack_archive(r"{}/{}_archive.{}".format(self.top_path, self.name, self.pack_type))
-
+        # TODO: rename the recovered archive file
+        shutil.unpack_archive(r"{}/Coding/{}_donut_decoded.{}".format(os.getcwd(), self.pkg_name, self.pack_type))
--- donut/block_manager_linux/collector.py
+++ donut/block_manager_linux/collector.py
@@ -8,22 +8,22 @@ import re
 class Collector(object):
     """a class make a deb package from local
 
-    :param package_name: the needed package's name
+    :param package_name: gcc-8-base
     """
-
     def __init__(self, package_name):
-        self.package_name = package_name
+        self.package_name = package_name  # gcc-8-base
         self.top_path = os.getcwd()  # the temp file all exist in current path
-        self.install_list_path = "{}/installed.csv".format(self.top_path)
-        self.location_file_path = "{}/location".format(self.top_path)
+        self.installed_file = "{}/installed.csv".format(self.top_path)  # stay in top_path
+        self.location_file = "{}/location".format(self.top_path)  # temp file
+        self.temp_package_folder = "{}/donut".format(self.top_path)
 
     def extract_installed_package(self):
         """get the list of package installed, and store the list into a json file
         this json file will store forever, and update after every package install or uninstall"""
-        temp_install = "{}/installed".format(self.top_path)
-        command = "dpkg -l > {}".format(temp_install)
+        temp_installed_file = "{}/installed".format(self.top_path)
+        command = "dpkg -l > {}".format(temp_installed_file)
         os.system(command)
-        with open(temp_install, 'r') as f:
+        with open(temp_installed_file, 'r') as f:
             list = f.readlines()
             for index, content in enumerate(list):
                 if "+++-===" in content:
@@ -41,13 +41,13 @@ class Collector(object):
         data_1 = [functools.reduce(lambda x, y: x + ' ' + y, content[3:]) for content in data_0]
         data_2 = [content[:3] for content in data_0]
         data = [x + [y] for x, y in list(zip(data_2, data_1))]
-        with open(self.install_list_path, 'w', newline='') as file:
+        with open(self.installed_file, 'w', newline='') as file:
             csv_writer = csv.writer(file)
             csv_writer.writerow(header)
             csv_writer.writerows(data)
 
     def package_exist(self):
-        with open(self.install_list_path, 'r') as file:
+        with open(self.installed_file, 'r') as file:
             csv_reader = csv.DictReader(file.readlines())
             for package in csv_reader:
                 if package['Name'] == self.package_name:
@@ -56,11 +56,11 @@ class Collector(object):
 
     def collector_file(self):
         """get the information from system, and collect them"""
-        command = "dpkg-query -L {} > {}".format(self.package_name, self.location_file_path)
+        command = "dpkg-query -L {} > {}".format(self.package_name, self.location_file)
         os.system(command)
         file_list = []
         folder_list = []
-        with open(self.location_file_path, 'r') as file:
+        with open(self.location_file, 'r') as file:
             path = file.readlines()
             for p in path[1:]:
                 file_list.append(p.strip())
@@ -75,8 +75,7 @@ class Collector(object):
             file_list.remove('')
         self.copy_file(set(file_list), set(folder_list))
 
-    @staticmethod
-    def copy_file(file_list, folder_list):
+    def copy_file(self, file_list, folder_list):
         """mkdir and copy
 
         :param folder_list:
@@ -85,17 +84,17 @@ class Collector(object):
         :type folder_list: set
         """
         folder_list = sorted(folder_list, key=lambda x: len(x))
-        if os.path.exists("test"):
-            shutil.rmtree("test")
-        os.mkdir('test')
+        if os.path.exists(self.temp_package_folder):
+            shutil.rmtree(self.temp_package_folder)
+        # TODO: add error handle for asking new temp_package_folder name
+        os.mkdir(self.temp_package_folder)
         for folder in folder_list:
-            os.mkdir("test{}".format(folder))
+            os.mkdir("{}{}".format(self.temp_package_folder,folder))
         for file in file_list:
             if os.path.isfile(file):
-                shutil.copy(src=file, dst="test{}".format(file))
+                shutil.copy(src=file, dst="{}{}".format(self.temp_package_folder, file))
             else:
-                # os.mkdir("test{}".format(file))
-                shutil.copytree(src=file, dst="test{}".format(file))
+                shutil.copytree(src=file, dst="{}{}".format(self.temp_package_folder, file))
 
     # TODO: fix the bug
     def extract_control(self, version=None):
@@ -125,9 +124,8 @@ class Collector(object):
     def get_control(self):
         command = 'apt-cache show {} > control_temp'.format(self.package_name)
         os.system(command)
-        f = open('control_temp', 'r')
-        control_info = f.read()
-        f.close()
+        with open('control_temp', 'r') as f:
+            control_info = f.read()
         return control_info
 
     def extract_md5(self):
@@ -137,33 +135,38 @@ class Collector(object):
         with open("where_md5", 'r') as md5:
             md5 = md5.read().strip()
             if os.path.exists(md5):
-                command = "cat {} > {}".format(md5, "test/DEBIAN/md5sums")
+                command = "cat {} > {}".format(md5, "donut/DEBIAN/md5sums")
                 os.system(command)
             else:
                 print("Error")
+        os.remove("where_md5")
 
-    def build(self):
-        command = "dpkg-deb -b {} donut_test_{}.deb".format("test", self.package_name)
+    @staticmethod
+    def build(temp_folder, deb_name):
+        """build deb package
+
+        :param temp_folder: top_path/donut
+        :param deb_name: {package_name}_donut.deb
+        """
+        # TODO: add file existence check
+        command = "dpkg-deb -b {} {}".format(temp_folder, deb_name)
         os.system(command)
+        print("Donut--> build successful!")
 
     @staticmethod
     def fix_control(file_name):
-        file = open(file_name, 'r+')
-        data = file.readlines()
-        file.close()
-        file = open(file_name, 'w')
-        for line in data:
-            if('Status: ' in line):
-                continue
-            file.write(line)
-        file.write("\n")
-        file.close()
+        with open(file_name, 'r+') as file:
+            data = file.readlines()
+        with open(file_name, 'w') as file:
+            for line in data:
+                if 'Status: ' in line:
+                    continue
+                file.write(line)
+            file.write("\n")
 
     def clean(self):
-        os.remove("installed")
-        os.remove("where_md5")
-        os.remove("location")
+        os.remove("installed")  # install temp
+        os.remove(self.location_file)
         os.remove("control_temp")
-        if os.path.exists("test"):
-            shutil.rmtree("test")
-
+        if os.path.exists(self.temp_package_folder):
+            shutil.rmtree(self.temp_package_folder)
